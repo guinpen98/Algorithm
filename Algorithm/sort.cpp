7a322c2a6ca37a676dadbdb7b13b400a0bcf6e7a
guinpen98 <83969826+guinpen98@users.noreply.github.com>
2022-11-06 21:10:40 +0900

    Implement BinarySearch

---
+++ Algorithm/sort.cpp
@@ -0,0 +1,16 @@
+#include "sort.h"
+#include <cmath>
+
+int Sort::BinarySearch(const std::vector<int>& vector, const int x)
+{
+	int left = 0;
+	int right = (int)vector.size();
+	int center = std::floor((left + right) / 2);
+	while (left + 1 < right) {
+		center = std::floor((left + right) / 2);
+		if (x < vector[center]) right = center;
+		else left = center;
+	}
+	if (vector[left] == x) return left;
+	else return -1;
+}
---
+++ Algorithm/sort.h
@@ -0,0 +1,11 @@
+#ifndef ALGORITHM_SORT_H
+#define ALGORITHM_SORT_H
+#include <vector>
+
+class Sort
+{
+public:
+	static int BinarySearch(const std::vector<int>& vector,const int x);
+};
+#endif //!ALGORITHM_SORT_H
+
